3ac60aa579c35339aaf3984211be1a812c91100b
MS08-067 <709046532@qq.com>
2021-06-22 20:51:00 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -117,22 +117,3 @@ texinfo_documents = [
      author, 'Web-Sec', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-latex_elements = {
-    'papersize': 'a4paper',
-    'preamble': '''
-    \hypersetup{unicode=true}
-    \usepackage{CJKutf8}
-    \DeclareUnicodeCharacter{00A0}{\\nobreakspace}
-    \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
-    \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
-    \DeclareUnicodeCharacter{2713}{x}
-    \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
-    \DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
-    \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
-    \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
-    \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
-    \\begin{CJK}{UTF8}{gbsn}
-    \AtEndDocument{\end{CJK}}
-    '''
-}
